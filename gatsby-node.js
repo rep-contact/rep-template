f677d090ab6715ebb902b239f4a8bf2b5a27cffe
Matthew Beal <matt@butcherbox.com>
2020-06-21 12:29:35 -0400

    working on generating pages based on primsic nodes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// You can delete this file if you're not using it
+// exports.onCreateNode = ({ node, actions }) => {
+//   if (node.internal.type === "PrismicTalkingPoint") {
+//     const { createNodeField } = actions;
+
+//     // const fileNode = getNode(node.parent);
+//     // console.log(`\n`, fileNode.relativePath);
+//     var slug = "talking-point/" + node.slugs[0];
+//     createNodeField({ node, name: "slug", value: slug });
+//   }
+// };
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+
+// Query all Pages with their IDs and template data.
+const pages = await graphql(`
+  {
+    allPrismicTalkingPoint {
+      nodes {
+        id
+        uid
+      }
+    }
+  }
+`);
+
+const pageTemplates = {
+  Light: path.resolve(__dirname, "src/templates/light.js"),
+  Dark: path.resolve(__dirname, "src/templates/dark.js"),
+};
+
+// Create pages for each Page in Prismic using the selected template.
+pages.data.allPrismicPage.nodes.forEach((node) => {
+  createPage({
+    path: `/${node.uid}`,
+    component: pageTemplates[node.template],
+    context: {
+      id: node.id,
+    },
+  });
+});
---
+++ src/templates/post.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { graphql } from "gatsby"
+function BlogPostTemplate(props) {
+  const post = props.data.markdownRemark
+  return (
+    <div>
+      <h1>{post.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    </div>
+  )
+}
+export default BlogPostTemplate
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
