92b66b0601b9d4f79cca13ec4840cbd13297c870
Matthew Beal <matt@butcherbox.com>
2020-06-27 16:45:34 -0400

    updated gatsby cli and implement query for google api

---
+++ src/pages/search.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import Layout from "../components/layout";
+// import Image from "../components/image";
+// import SEO from "../components/seo";
+
+const IndexPage = () => {
+  // Build Time Data Fetching
+  //   const gatsbyRepoData = useStaticQuery(graphql`
+  //     query {
+  //       github {
+  //         repository(name: "gatsby", owner: "gatsbyjs") {
+  //           id
+  //           nameWithOwner
+  //           url
+  //         }
+  //       }
+  //     }
+  //   `);
+  // Client-side Runtime Data Fetching
+  const [reps, setReps] = useState({});
+  const formData = {
+    address: "",
+  };
+
+  const key = "AIzaSyCZHujzaczbqT71RKxHvmbefgqTSr5Z65I";
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const address = formData.address;
+    const endpoint = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${address}`;
+
+    fetch(endpoint)
+      //   .then((response) => response.json()) // parse JSON from request
+      .then((response) => {
+        console.log(response.json());
+        // setReps(resultData);
+      });
+    // set data for the number of stars
+  };
+
+  const handleChange = (event) => {
+    formData.address = event.target.value;
+  };
+
+  return (
+    <Layout>
+      <section>
+        <form>
+          <label>
+            Address
+            <input type="text" name="address" onChange={handleChange} />
+          </label>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+
+        {/* <p>Runtime Data: Star count for the Gatsby repo {starsCount}</p> */}
+      </section>
+    </Layout>
+  );
+};
+export default IndexPage;
