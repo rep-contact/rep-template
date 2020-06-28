1820761f8a2765cc98931842f5c1d989cb38b49b
Matthew Beal <matt@butcherbox.com>
2020-06-28 07:47:21 -0400

    testing

--- functions/civicsCall.js
+++ functions/civicsCall.js
@@ -1,6 +1,11 @@
+import fetch from "node-fetch";
+
 exports.handler = (event, context, callback) => {
+  const googleApiUrl = `https://www.googleapis.com/civicinfo/v2/representatives`;
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const address = event.body.address;
   callback(null, {
     statusCode: 200,
-    body: "No worries, all is working fine!",
+    body: apiKey && address + googleApiUrl,
   });
 };
