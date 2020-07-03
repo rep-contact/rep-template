658610d684ad35c90af8b3b4f72c2cb62707316d
Matthew Beal <matt@butcherbox.com>
2020-07-03 09:05:45 -0400

    function working again

--- functions/civicsCall.js
+++ functions/civicsCall.js
@@ -1,14 +1,13 @@
 const axios = require("axios");
 
-exports.handler = async (event, callback) => {
+exports.handler = async (event, context, callback) => {
   const address = event.body;
   const key = process.env.GOOGLE_API_KEY;
   const endpoint = `https://www.googleapis.com/civicinfo/v2/representatives?key=${key}&address=${address}`;
 
   const res = await axios.get(endpoint);
-
   callback(null, {
-    statusCode: res.status,
+    statusCode: 200,
     body: JSON.stringify({ response: res.data }),
   });
 };
