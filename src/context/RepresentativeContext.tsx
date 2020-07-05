9cbdcd72412774b00b2fb1cfc78a1cbbcfb1edcc
Matthew Beal <matt@butcherbox.com>
2020-07-05 19:48:56 -0400

    removed comment

--- src/context/RepresentativeContext.tsx
+++ src/context/RepresentativeContext.tsx
@@ -1,51 +1,5 @@
 import React from "react";
 
-// const defaultState = {
-//   representativeData: {},
-//   addRepresentativeData: (data) => {},
-// };
+const { Provider, Consumer } = React.createContext({});
 
-// const RepresentativeContext = React.createContext(defaultState);
-
-// class RepresentativeProvider extends React.Component {
-//   state = {
-//     representativeData: {},
-//   };
-
-//   addRepresentativeData = (data) => {
-//     this.setState({ data });
-//   };
-
-//   //   componentDidMount() {
-//   //     // Getting dark mode value from localStorage!
-//   //     const lsDark = JSON.parse(localStorage.getItem("dark"));
-//   //     if (lsDark) {
-//   //       this.setState({ dark: lsDark });
-//   //     } else if (supportsDarkMode()) {
-//   //       this.setState({ dark: true });
-//   //     }
-//   //   }
-
-//   render() {
-//     const { children } = this.props;
-//     const { representativeData } = this.state;
-//     return (
-//       <RepresentativeContext.Provider
-//         value={{
-//           representativeData,
-//           addRepresentativeData: this.addRepresentativeData,
-//         }}
-//       >
-//         {children}
-//       </RepresentativeContext.Provider>
-//     );
-//   }
-// }
-
-// export default RepresentativeContext;
-
-// export { RepresentativeProvider };
-
-const {Provider, Consumer} = React.createContext({})
-
-export {Provider, Consumer}
+export { Provider, Consumer };
