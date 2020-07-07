189b14b1ef1b451798225f394e3791faa8b1d61c
Matthew Beal <matt@butcherbox.com>
2020-07-06 23:15:33 -0400

    implemented default pricing template accross pages, moved my search page to be my index

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,124 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from "react";
+import {
+  CssBaseline,
+  Toolbar,
+  Typography,
+  AppBar,
+  Link,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+}));
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+const Header = () => {
+  const classes = useStyles();
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            TellMyRep
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              I
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              Luv
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="#"
+              className={classes.link}
+            >
+              U
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+// <header
+//   style={{
+//     background: `rebeccapurple`,
+//     marginBottom: `1.45rem`,
+//   }}
+// >
 
-export default Header
+//   <div
+//     style={{
+//       margin: `0 auto`,
+//       maxWidth: 960,
+//       padding: `1.45rem 1.0875rem`,
+//     }}
+//   >
+//     <h1 style={{ margin: 0 }}>
+//       <Link
+//         to="/"
+//         style={{
+//           color: `white`,
+//           textDecoration: `none`,
+//         }}
+//       >
+//         {siteTitle}
+//       </Link>
+//     </h1>
+//   </div>
+// </header>
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// };
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// };
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,164 @@
 import React from "react";
-import { Link } from "gatsby";
-import { Button } from "@material-ui/core";
-
 import Layout from "../components/layout";
-import Image from "../components/image";
-import SEO from "../components/seo";
+import { Consumer } from "../context/RepresentativeContext";
+import {
+  Container,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  FormHelperText,
+  Input,
+} from "@material-ui/core";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import { makeStyles } from "@material-ui/core/styles";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Button to="/page-2/">Go to page 2</Button> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-);
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+}));
 
-export default IndexPage;
+const SearchPage = () => {
+  const formData = {
+    address: "",
+  };
+
+  const classes = useStyles();
+
+  const handleSubmit = (event, setReps) => {
+    event.preventDefault();
+    const address = formData.address;
+
+    fetch("/.netlify/functions/civicsCall", {
+      method: "POST",
+      body: address,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json()) // parse JSON from request
+      .then((resultData) => {
+        setReps(resultData.response);
+      });
+  };
+
+  const handleChange = (event) => {
+    formData.address = event.target.value;
+  };
+
+  return (
+    <Consumer>
+      {(context) => (
+        <Layout>
+          {/* Hero unit */}
+          <Container
+            maxWidth="sm"
+            component="main"
+            className={classes.heroContent}
+          >
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              An Easy Way to Reach Out
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              component="p"
+            >
+              Quickly reach out to your representatives and demand change within
+              your Community, State, and Country
+            </Typography>
+          </Container>
+          {/* End hero unit */}
+          <Container maxWidth="md" component="main">
+            <Grid container spacing={5} alignItems="flex-end">
+              <Card>
+                <CardHeader
+                  title="Contact"
+                  subheader="Enter your Zip code to get your reps"
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <Consumer>
+                    {(context) => (
+                      <FormControl textAlign="center">
+                        <InputLabel htmlFor="my-input">ZipCode</InputLabel>
+                        <Input
+                          id="my-input"
+                          aria-describedby="my-helper-text"
+                          onChange={handleChange}
+                        />
+                        <FormHelperText id="my-helper-text">
+                          We'll never share your address.
+                        </FormHelperText>
+                        <Button
+                          onClick={(e) => handleSubmit(e, context.setReps)}
+                        >
+                          Submit
+                        </Button>
+                      </FormControl>
+                    )}
+                  </Consumer>
+                </CardContent>
+              </Card>
+              <ArrowForwardIcon />
+              <Card>
+                <CardHeader
+                  title="Your Reps"
+                  subheader="Your reps will show up here"
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <ul>
+                    {context.reps &&
+                      context.reps.officials.map((official) => (
+                        <li>{official.name}</li>
+                      ))}
+                  </ul>
+
+                  {/* <>{JSON.stringify(context.reps.officials)}</> */}
+                </CardContent>
+              </Card>
+            </Grid>
+          </Container>
+        </Layout>
+      )}
+    </Consumer>
+  );
+};
+export default SearchPage;
